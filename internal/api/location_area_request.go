49c6d59ae1a71e0fbe519ce0e84337fa62119978
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2023-11-29 09:05:05 +0000

    feat(explore command): WIP - added new explore command + function to get location data;

--- internal/api/location_area_request.go
+++ internal/api/location_area_request.go
@@ -65,3 +65,58 @@ func (c *Client) ListLocationAreas(pageUrl *string) (LocationAreasResponse, erro
 
 	return locationAreasResp, nil
 }
+
+func (c *Client) GetLocationArea(locationAreaName string) (LocationArea, error) {
+	endpoint := "/location-area" + locationAreaName
+	fullUrl := baseUrl + endpoint
+
+	// check the cache
+	data, ok := c.cache.Get(fullUrl)
+	if ok {
+		// cache hit
+		fmt.Println("cache hit")
+		locationArea := LocationArea{} // where we store the data
+		// unmarshalling the data: taking a pointer to a structure and filling in the data
+		err := json.Unmarshal(data, &locationArea)
+		if err != nil {
+			return LocationArea{}, err
+		}
+		return locationArea, nil // if there is a cache hit, return successfully early
+	}
+	fmt.Println("cache miss")
+
+	// request
+	req, err := http.NewRequest("GET", fullUrl, nil)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	// response
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	defer resp.Body.Close()
+
+	// for 400 (bad req), 500 (server err) responses
+	if resp.StatusCode > 399 {
+		return LocationArea{}, fmt.Errorf("bad status code: %v", resp.StatusCode)
+	}
+
+	data, err = io.ReadAll(resp.Body) // read the data
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	locationArea := LocationArea{} // where we store the data
+	// unmarshalling the data: taking a pointer to a structure and filling in the data
+	err = json.Unmarshal(data, &locationArea)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	c.cache.Add(fullUrl, data)
+
+	return locationArea, nil
+}
